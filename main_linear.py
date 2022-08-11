a544457096e7c793861b25297e9de35ad32dd5a5
a <tengyangxie@users.noreply.github.com>
2022-08-10 22:40:39 -0500

    .

--- main_linear.py
+++ main_linear.py
@@ -51,7 +51,6 @@ def parse_option():
     parser.add_argument('--model', type=str, default='resnet50')
     parser.add_argument('--dataset', type=str, default='cifar10',
                         choices=['cifar10', 'cifar100'], help='dataset')
-    parser.add_argument('--size', type=int, default=32, help='parameter for RandomResizedCrop')
 
     # other setting
     parser.add_argument('--cosine', action='store_true',
